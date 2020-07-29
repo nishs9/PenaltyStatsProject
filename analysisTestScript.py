3bac6a37f0f73c39a0acdc8a47edeebb4b9b371b
Nishant Sethunath <36416402+nishs9@users.noreply.github.com>
2020-07-29 14:08:21 -0400

    Update analysisTestScript.py

--- analysisTestScript.py
+++ analysisTestScript.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import csv
 import matplotlib.pyplot as plt
+import numpy as numpy
+from scipy import stats
 
 seasonReportRaw = pd.read_csv("Data/19-20 ARI/ARI_SeasonRaw.csv")
 
@@ -8,6 +10,6 @@ seasonReportRaw['Date'] = seasonReportRaw['Date'].astype('datetime64[ns]')
 
 seasonReportRaw['tEPPfP'] = seasonReportRaw['tEPDHP'] + seasonReportRaw['tEPDEP'] + seasonReportRaw['tEPDOP']
 
-print(seasonReportRaw.plot(x='Date',y=['tEPPfP','tEPDHP','tEPDEP','tEPDOP']))
+seasonReportRaw.plot(x="Date",y=['tPEN(#)','tEPPfP'])
 
 plt.show()
