56c1d82cad8dbffef18c799d9cc11e3b2e789747
Nishant Sethunath <36416402+nishs9@users.noreply.github.com>
2020-07-30 12:53:55 -0400

    Update dataAnalysisVizScript.py

--- dataAnalysisVizScript.py
+++ dataAnalysisVizScript.py
@@ -20,8 +20,27 @@ def tPEN_tEPP_graph(year,team):
 
 	plt.savefig("Analysis/" + year + " " + team + "/" + team + "_tPENtEPPgraph.png",dpi=400)
 
+def allExpPoints_graph(year,team):
+	seasonReportRaw = pd.read_csv("Data/" + year + " " + team + "/" + team + "_SeasonRaw.csv")
+
+	seasonReportRaw['Date'] = seasonReportRaw['Date'].astype('datetime64[ns]')
+
+	seasonReportRaw['tEPPfP'] = seasonReportRaw['tEPDHP'] + seasonReportRaw['tEPDEP'] + seasonReportRaw['tEPDOP']
+
+	seasonReportRaw.plot(x="Date",y=['tEPPfP','tEPDHP','tEPDEP','tEPDOP'])
+
+	plotLegend = plt.legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=2)
+
+	plotLegend.get_texts()[0].set_text('EPC due to Penalties')
+	plotLegend.get_texts()[1].set_text('EPC due to DHPs')
+	plotLegend.get_texts()[2].set_text('EPC due to DEPs')
+	plotLegend.get_texts()[3].set_text('EPC due to OPs')
+
+	plt.savefig("Analysis/" + year + " " + team + "/" + team + "_allExpPointsgraph.png",dpi=400)
+
 if __name__ == '__main__':
 	year = "19-20"
 	team = "ARI"
 
 	tPEN_tEPP_graph(year,team)
+	allExpPoints_graph(year,team)
