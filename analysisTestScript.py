9733246a639e4e4722d6805cc6412e903b93ba33
Nishant Sethunath <36416402+nishs9@users.noreply.github.com>
2020-07-30 16:32:18 -0400

    Working on league-wide data analysis functions

--- analysisTestScript.py
+++ analysisTestScript.py
@@ -1,19 +1,21 @@
 import os
 import location
+import teamList
 import pandas as pd
 import csv
 import matplotlib.pyplot as plt
 import numpy as numpy
 from scipy import stats
 
+##Scatterplot of Wins vs total expected points
 
+directory = "C:/Users/" + location.location + "/Desktop/PenaltyStatsProject/Data/19-20 ARI"
 
-seasonReportRaw = pd.read_csv("Data/19-20 ARI/ARI_SeasonRaw.csv")
+for filename in os.listdir(directory):
+	csvLoc = directory + "/" + filename
+	
+	if filename != team + "_SeasonRaw.csv":
+		continue
+	
+	seasonReportRaw = pd.read_csv(csvLoc)
 
-seasonReportRaw['Date'] = seasonReportRaw['Date'].astype('datetime64[ns]')
-
-seasonReportRaw.boxplot(column=['tPEN(#)','tDHP(#)','tDEP(#)','tOP(#)'])
-
-plt.savefig('test.png',dpi=400)
-
-plt.show()
