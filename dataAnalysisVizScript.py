47928ae59af41ebb5f1d9ef841ce9f2522ef0983
Nishant Sethunath <36416402+nishs9@users.noreply.github.com>
2020-07-30 11:28:58 -0400

    Update dataAnalysisVizScript.py

--- dataAnalysisVizScript.py
+++ dataAnalysisVizScript.py
@@ -4,6 +4,23 @@ import matplotlib.pyplot as plt
 import numpy as numpy
 from scipy import stats
 
+year = "19-20"
+team = "ARI"
 
+def tPEN_tEPP_graph(year,team):
+	seasonReportRaw = pd.read_csv("Data/" + year + " " + team + "/" + team + "_SeasonRaw.csv")
+
+	seasonReportRaw['Date'] = seasonReportRaw['Date'].asType('datetime64[ns]')
+
+	seasonReportRaw['tEPPfP'] = seasonReportRaw['tEPDHP'] + seasonReportRaw['tEPDEP'] + seasonReportRaw['tEPDOP']
+
+	seasonReportRaw.plot(x='Date',y=['tPEN(#)','tEPPfP'])
+
+	plotLegend = plt.legend()
+
+	plotLegend.get_texts()[0].set_text('# of Penalties')
+	plotLegend.get_texts()[1].set_text('EPC due to Penalties')
+
+	plt.savefig("Data/" + year + " " + team + "/" + team + "_tPENtEPPgraph.png",dpi=400)
 
 if __name__ == '__main__':
