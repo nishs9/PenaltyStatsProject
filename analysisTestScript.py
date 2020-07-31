dabcc2d7e705e68cb8f17ec03fb57c828212a592
Nishant Sethunath <36416402+nishs9@users.noreply.github.com>
2020-07-31 11:09:36 -0400

    Adding functionality for league-wide data analysis and visualizations

--- analysisTestScript.py
+++ analysisTestScript.py
@@ -5,12 +5,18 @@ import pandas as pd
 import csv
 import matplotlib.pyplot as plt
 import numpy as numpy
-from scipy import stats
+from scipy.stats import linregress
 
 ##Scatterplot of Wins vs total expected points
 
+
+## { 'Team' : (Wins,Total Expected Points) }
+seasonInfoDict = {}
+
 for team in teamList.teamList:	
 	directory = "C:/Users/" + location.location + "/Desktop/PenaltyStatsProject/Data/19-20 " + team 
+
+	seasonInfoDict[team] = ()
 
 	for filename in os.listdir(directory):
 		csvLoc = directory + "/" + filename
@@ -19,5 +25,18 @@ for team in teamList.teamList:
 			continue
 		
 		seasonReportRaw = pd.read_csv(csvLoc)
-		print(seasonReportRaw['tPEN(#)'].sum())
+
+		homewins = seasonReportRaw[(seasonReportRaw["Home Team"] == team) & (seasonReportRaw["Home Score"] > seasonReportRaw["Away Score"])]
+
+		awaywins = seasonReportRaw[(seasonReportRaw["Away Team"] == team) & (seasonReportRaw["Away Score"] > seasonReportRaw["Home Score"])]
+
+		##print(str(homewins["Home Team"].count() + awaywins["Away Team"].count()))
+
+		seasonReportRaw['tEPPfP'] = seasonReportRaw['tEPDHP'] + seasonReportRaw['tEPDEP'] + seasonReportRaw['tEPDOP']
+
+		seasonInfoDict[team] = (homewins + awaywins, seasonReportRaw['tEPPFP'].sum())
+
+		##print(seasonReportRaw['tPEN(#)'].sum())
+		##print(seasonReportRaw['tEPPfP'].sum())
+		print("\n")
 
