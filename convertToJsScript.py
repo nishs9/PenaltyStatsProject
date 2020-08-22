8c049ef9dae140db22f75abd2f2231a3f4decef6
nishs9 <snishk@gmail.com>
2020-08-21 20:37:43 -0400

    Update convertToJsScript.py

--- convertToJsScript.py
+++ convertToJsScript.py
@@ -2,20 +2,25 @@ import csv
 import os
 import teamList
 
-seasonReportCsvLoc = 'C:/Users/snish/Desktop/PenaltyStatsProject/Data/18-19 ARI/ARI_SeasonWeb.csv'
-seasonReportTxtLoc = 'C:/Users/snish/Desktop/PenaltyStatsWebsite/nextjs-blog/public/data/ARI_SeasonWebJS.txt'
+years = ["18-19", "19-20"]
 
-with open(seasonReportTxtLoc, 'w') as seasonReportTxt:
-    seasonReportTxt.write("const text = `")
-    with open(seasonReportCsvLoc, 'r') as seasonReportCsv:
-        for row in csv.reader(seasonReportCsv):
-            seasonReportTxt.write(",".join(row) + '\n')
-    seasonReportTxt.write("`;")
-    seasonReportTxt.write("\n")
-    seasonReportTxt.write("\n")
-    seasonReportTxt.write("export default text;")
+for year in years:
+    for team in teamList.teamList:
+        folder = year + " " + team
+        seasonReportCsvLoc = 'C:/Users/snish/Desktop/PenaltyStatsProject/Data/' + folder + '/' + team + '_SeasonWeb.csv'
+        seasonReportTxtLoc = 'C:/Users/snish/Desktop/PenaltyStatsWebsite/nextjs-blog/public/data/' + year + team + '_SeasonWebJS.txt'
 
-    pre, ext = os.path.splitext(seasonReportTxtLoc)
+        with open(seasonReportTxtLoc, 'w') as seasonReportTxt:
+            seasonReportTxt.write("const text = `")
+            with open(seasonReportCsvLoc, 'r') as seasonReportCsv:
+                for row in csv.reader(seasonReportCsv):
+                    seasonReportTxt.write(",".join(row) + '\n')
+            seasonReportTxt.write("`;")
+            seasonReportTxt.write("\n")
+            seasonReportTxt.write("\n")
+            seasonReportTxt.write("export default text;")
 
-os.rename(seasonReportTxtLoc, pre + ".js")
+            pre, ext = os.path.splitext(seasonReportTxtLoc)
+
+        os.rename(seasonReportTxtLoc, pre + ".js")
 
