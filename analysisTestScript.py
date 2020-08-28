c10b2b0fc903c8b82a3b28c4fd9d3424561ae0be
nishs9 <snishk@gmail.com>
2020-08-28 11:25:47 -0400

    working on creating summary stats table for the team page on the website

--- analysisTestScript.py
+++ analysisTestScript.py
@@ -7,20 +7,69 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
 
-year="18-19"
-team="ARI"
+year = "18-19"
+
+team = "ARI"
 
 seasonReportRaw = pd.read_csv("Data/" + year + " " + team + "/" + team + "_SeasonRaw.csv")
 
-seasonReportRaw['Date'] = seasonReportRaw['Date'].astype('datetime64[ns]')
-
-seasonReportRaw['tEPPfP'] = seasonReportRaw['tEPDHP'] + seasonReportRaw['tEPDEP'] + seasonReportRaw['tEPDOP']
-
-seasonReportRaw = seasonReportRaw.rename(columns={'tEPPfP':'tEPCP','tEPDHP':'tEPCDHP','tEPDEP':'tEPCDEP','tEPDOP':'tEPCOP'})
-
-print(seasonReportRaw)
-
-seasonReportRaw.boxplot(column=['tEPCP','tEPCDHP','tEPCDEP','tEPCOP'])
-
-plt.savefig("Analysis/" + year + " " + team + "/" + team + "_allExpPointsboxplot.png",dpi=400,bbox_inches="tight")
-plt.clf()
+title_text = 'Penalty Stats Summary'
+fig_background_color = 'white'
+fig_border = 'black'
+data =  [
+            [         'Season Total', 'Per Game', 'Win Total', 'Per Win', 'Loss Total','Per Loss'],
+            [ 'Penalties',  66386, 174296,   75131,  577908,  32015, 32015],
+            ['EPC',  58230, 381139,   78045,   99308, 160454, 160454],
+            ['EPCDHP',  89135,  80552,  152558,  497981, 603535, 603535],
+            ['EPCDEP',  78415,  81858,  150656,  193263,  69638, 69638],
+            ['EPCOP', 139361, 331509,  343164,  781380,  52269, 52269],
+        ]
+# Pop the headers from the data array
+column_headers = data.pop(0)
+row_headers = [x.pop(0) for x in data]
+# Table data needs to be non-numeric text. Format the data
+# while I'm at it.
+cell_text = []
+for row in data:
+    cell_text.append([f'{x:1.1f}' for x in row])
+# Get some lists of color specs for row and column headers
+rcolors = plt.cm.BuPu(np.full(len(row_headers), 0.1))
+ccolors = plt.cm.BuPu(np.full(len(column_headers), 0.1))
+# Create the figure. Setting a small pad on tight_layout
+# seems to better regulate white space. Sometimes experimenting
+# with an explicit figsize here can produce better outcome.
+plt.figure(linewidth=2,
+           edgecolor=fig_border,
+           facecolor=fig_background_color,
+           tight_layout={'pad':1},
+           figsize=(5,2)
+          )
+# Add a table at the bottom of the axes
+the_table = plt.table(cellText=cell_text,
+                      rowLabels=row_headers,
+                      rowColours=rcolors,
+                      rowLoc='right',
+                      colColours=ccolors,
+                      colLabels=column_headers,
+                      loc='center')
+# Scaling is the only influence we have over top and bottom cell padding.
+# Make the rows taller (i.e., make cell y scale larger).
+the_table.scale(1, 1.1)
+# Hide axes
+ax = plt.gca()
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+# Hide axes border
+plt.box(on=None)
+# Add title
+plt.suptitle(title_text)
+# Force the figure to update, so backends center objects correctly within the figure.
+# Without plt.draw() here, the title will center on the axes and not the figure.
+plt.draw()
+# Create image. plt.savefig ignores figure edge and face colors, so map them.
+fig = plt.gcf()
+plt.savefig('pyplot-table-demo.png',
+            edgecolor=fig.get_edgecolor(),
+            facecolor=fig.get_facecolor(),
+            dpi=175
+            )
